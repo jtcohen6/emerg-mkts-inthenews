45da2b63f6f20e89fc795961f1a404f93ba4fb7a
raghavjoshi <joshir@seas.upenn.edu>
2016-01-23 03:52:48 -0500

    Added requirements.txt (gotta fill this ish out). Main.py will be pandas dataframe containing tickers + etfs and then blackrock api will be called. Magic will happen

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import nltk
+import pandas
+
+
+"""
+Open csv files, and create pandas dataframe with matchings between title and etf ticker symbol.
+Goal is to use this with Blackrock API and get relevant information
+Goal is to then visualize using Beaker/c3.js/d3.js
+"""
+
+
+
+if __name__ == '__main__':
+	main()
