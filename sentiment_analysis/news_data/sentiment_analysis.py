ab2608fa99b462b17f1067bab42425bdc22a90ac
raghavjoshi <joshir@seas.upenn.edu>
2016-01-24 07:00:22 -0500

    Big commit. Random and non random sentiment files

--- sentiment_analysis/news_data/sentiment_analysis.py
+++ sentiment_analysis/news_data/sentiment_analysis.py
@@ -2,14 +2,20 @@
 
 import pandas as pd 
 from vaderSentiment import vaderSentiment
+import random
 
+
+def random_sample(df_size, limit):
+	random.seed(100)
+	return random.sample(xrange(df_size), limit)
 
 #need to access pandas data frame (start and stop)
-def main(csv_file, txt_file, limit, name):
+def main(csv_file, txt_file, amount, name):
 	with open(txt_file) as input_file:
 		data = pd.read_csv(csv_file)
-		data = data.head(limit)
-		df = pd.DataFrame(data[data.columns[1:5]])
+		rand_choices = random_sample(len(data), amount)
+		new_df = data.loc[[i for i in rand_choices]]
+		df = pd.DataFrame(new_df[new_df.columns[1:5]])
 		df["vader_score_neg"] = ""
 		df["vader_score_neu"] = ""
 		df["vader_score_pos"] = ""
@@ -20,7 +26,7 @@ def main(csv_file, txt_file, limit, name):
 		container = list(input_file)
 		for i,j in zip(start,end):
 			articles.append(''.join(container[i:j]))
-		for i in range(limit):
+		for i in df.index.values:
 			df.loc[i, 'vader_score_neg'] = vaderSentiment.sentiment(articles[i])['neg']
 			df.loc[i, 'vader_score_neu'] = vaderSentiment.sentiment(articles[i])['neu']
 			df.loc[i, 'vader_score_pos'] = vaderSentiment.sentiment(articles[i])['pos']
@@ -28,5 +34,5 @@ def main(csv_file, txt_file, limit, name):
 		df.to_csv(name+'_sentiment.csv')
 
 if __name__ == "__main__":
-	main("ftimes.csv","ftimes21494.txt", 100, "ftimes")
-	main("forbes.csv","forbes1220.ansi.txt", 100, "forbes" )
+	main("ftimes.csv","ftimes21494.txt", 100, "random_ftimes")
+	main("forbes.csv","forbes1220.ansi.txt", 100, "random_forbes" )
